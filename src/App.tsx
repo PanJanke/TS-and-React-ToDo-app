2f730c0b870fd3df7b1a8e5f8a5bd198c03586ba
Jan Krol <jan.krol.legowski@gmail.com>
2024-06-04 13:34:16 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import CourseGoal from "./components/CourseGoal.tsx";
+
+export default function App() {
+  return <main>
+    <CourseGoal
+         title="Learn React + TS">
+        <p>Learn it form the ground up</p>
+    </CourseGoal>
+
+  </main>;
+}
