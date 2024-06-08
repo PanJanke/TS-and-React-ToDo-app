d56c133e4d3570b31c51826768623cbf72783c6a
Jan Krol <jan.krol.legowski@gmail.com>
2024-06-08 20:06:31 +0200

    form handle logic

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,22 +1,35 @@
-import {type FormEvent} from "react";
-export default function NewGoal(){
-    function  handleSubmit(event: FormEvent){
+import {type FormEvent, useRef} from "react";
+
+type NewGoalProps = {
+    onAddGoal: (goal: string, summary: string) => void;
+};
+export default function NewGoal({onAddGoal}: NewGoalProps) {
+    const goal = useRef<HTMLInputElement>(null);
+    const summary = useRef<HTMLInputElement>(null);
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
 
+        const enteredGoal = goal.current!.value;
+        const enteredSummary = summary.current!.value;
+
+        event.currentTarget.reset();
+        onAddGoal(enteredGoal, enteredSummary);
     }
-    return(
+
+    return (
         <form onSubmit={handleSubmit}>
-        <p>
-            <label htmlFor="goal">Your Goal</label>
-            <input id="goal" type="text" />
-        </p>
-        <p>
-            <label htmlFor="summary">Short summary</label>
-            <input id="sumamry" type="text" />
-        </p>
-        <p>
-            <button>Add Goal</button>
-        </p>
-    </form>
+            <p>
+                <label htmlFor="goal">Your Goal</label>
+                <input id="goal" type="text" ref={goal}/>
+            </p>
+            <p>
+                <label htmlFor="summary">Short summary</label>
+                <input id="sumamry" type="text" ref={summary}/>
+            </p>
+            <p>
+                <button>Add Goal</button>
+            </p>
+        </form>
     );
 }
